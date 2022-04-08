31fef65d77982bdc9b30b789de61fd94c6e083dd
IcedOtaku <icedotaku.dev@gmail.com>
2022-04-08 16:29:48 +0800

    init: Add objc4-838 executable source code

---
+++ debug/main.m
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSObject *objc = [NSObject alloc];
+        NSLog(@"Debug objc4 Source Code: %@", objc);
+    }
+
+    return 0;
+}
